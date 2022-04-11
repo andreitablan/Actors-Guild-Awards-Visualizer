4b1b16e24d381b42ac96d11936039c3c209921b7
Andrie Tablan <andreitablan01@gmail.com>
2022-04-11 11:54:16 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Actors-Guild-Awards-Visualizer
+ Actors Guild Awards Visualizer is a project for Web Technologies. This project was created by Dan Leagan and Andrei Tablan.
---
+++ javascript.js
@@ -0,0 +1,36 @@
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const Burger = document.getElementsByClassName('burger')[0]
+
+toggleButton.addEventListener('click', () => {
+  Burger.classList.toggle('active')
+})
+
+  
+  var coll = document.getElementsByClassName("collapsible");
+  var i;
+  
+  for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.display === "block") {
+        content.style.display = "none";
+      } else {
+        content.style.display = "block";
+      }
+    });
+  }
+
+
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+  }
+  
