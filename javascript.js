2586ca9d63f9fece862a95758a20e572d163bdfd
Andrie Tablan <andreitablan01@gmail.com>
2022-04-12 22:23:58 +0300

    A little change

--- javascript.js
+++ javascript.js
@@ -34,3 +34,21 @@ function openNav() {
     document.getElementById("main").style.marginLeft = "0";
   }
   
+  /* THIS IS FOR THE CAROUSEL */
+  import Carousel from 'flat-carousel';
+
+const images = [
+    { src: 'some image' }
+];
+
+const MyCarousel = () => (
+    <Carousel>
+        {images.map((image, index) => (
+            <div
+                key={index}
+                className="demo-item"
+                style={{ backgroundImage: 'url(' + image.src + ')' }}
+            />
+        ))}
+    </Carousel>
+);
