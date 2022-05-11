e35bf936669d0c8b0c5d7ae92cda3623c250ecb1
Leagan-Dan <leaganndanut@gmail.com>
2022-05-11 22:14:46 +0300

    Create mongoBeginner.php

---
+++ Mockups/mongoBeginner.php
@@ -0,0 +1,41 @@
+<?php
+require 'vendor/autoload.php';
+
+$client = new MongoDB\Client;
+$companydb = $client->mongodb;
+$users=$companydb->users;
+
+//find one
+/*$document = $users->findOne(
+    ['show' => 'BOMBSHELL' ]
+);
+
+var_dump($document);
+*/
+
+
+//find all
+/*
+$documentList = $users->find(
+    ['won' => 'True']
+);
+
+foreach($documentList as $doc)
+{
+    var_dump($doc);
+}
+*/
+
+
+//find by criteria
+$documentList = $users->find(
+    ['won' => 'True','category' => 'CAST IN A MOTION PICTURE' ],
+    ['projection' => ['_id' => 0, 'full_name' => 1]]
+);
+
+foreach($documentList as $doc)
+{
+    var_dump($doc);
+}
+
+?>
