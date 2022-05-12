fc58969c3b414995d7f6036a9671141ab6696ccb
Leagan-Dan <leaganndanut@gmail.com>
2022-05-12 14:50:50 +0300

    Created Project_xampp

---
+++ Project_xampp/javascript.js
@@ -0,0 +1,34 @@
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const Burger = document.getElementsByClassName('burger')[0]
+
+toggleButton.addEventListener('click', () => {
+  Burger.classList.toggle('active')
+})
+
+  
+  var coll = document.getElementsByClassName("collapsible");
+  var i;
+  
+  for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.display === "block") {
+        content.style.display = "none";
+      } else {
+        content.style.display = "block";
+      }
+    });
+  }
+
+
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+  }
+  
